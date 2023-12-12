a0db03d3cd4ec11de31afc4b4755bf8644308307
syoder <syoder@imagestream.com>
2023-12-11 19:33:05 -0600

    fix: rewrite connection handler

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"time"
 	"os"
+	"os/signal"
+	"syscall"
+	"flag"
 	"encoding/json"
 	"github.com/VictoriaMetrics/metrics"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -30,7 +34,7 @@ var broker = "tcp://mosquitto:1883"
 // http://172.20.1.4:8428/api/v1/import/prometheus
 var vmPushURL = "http://victoria-metrics-victoria-metrics-single-server:8428/api/v1/import/prometheus"
 
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+func onMessageReceived(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 	json.Unmarshal([]byte(msg.Payload()), &tmsg)
 	fmt.Println(tmsg)
@@ -38,6 +42,9 @@ var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Me
 }
 
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
 	if val, ok := os.LookupEnv("SENSOR"); ok {
 		sensor = val
 	} else {
@@ -50,17 +57,18 @@ func main() {
 	if val, ok := os.LookupEnv("VM_PUSH_URL"); ok {
 		vmPushURL = val
 	}
+	qos := flag.Int("qos", 0, "The QoS to subscribe to messages at")
 
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(broker)
 	opts.SetClientID("monitor-"+sensor)
 	opts.SetUsername("emqx")
 	opts.SetPassword("public")
-	opts.SetDefaultPublishHandler(messagePubHandler)
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
+	opts.SetCleanSession(true)
+	opts.SetOrderMatters(false)
+	opts.SetKeepAlive(30 * time.Second)
+	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
+	opts.SetTLSConfig(tlsConfig)
 
 	metrics.NewGauge(`power`, func() float64 { return tmsg.ENERGY.Power })
 	metrics.NewGauge(`apparent_power`, func() float64 { return tmsg.ENERGY.ApparentPower })
@@ -69,19 +77,23 @@ func main() {
 	metrics.NewGauge(`voltage`, func() float64 { return tmsg.ENERGY.Voltage })
 	metrics.NewGauge(`current`, func() float64 { return tmsg.ENERGY.Current })
 
-	sub(client)
-
-	for true {
-		time.Sleep(time.Second)
+	topic := "tele/"+sensor+"/SENSOR"
+	opts.OnConnect = func(c mqtt.Client) {
+		if token := c.Subscribe(topic, byte(*qos), onMessageReceived); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+		fmt.Printf("Subscribed to topic: %s\n", topic)
 	}
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to %s\n", broker)
+	}
+
+	<-c
 }
 
 // Received message: {"Time":"2022-08-07T02:39:55","ENERGY":{"TotalStartTime":"2022-08-02T20:37:49","Total":0.006,"Yesterday":0.000,"Today":0.000,"Period": 0,"Power": 0,"ApparentPower": 0,"ReactivePower": 0,"Factor":0.00,"Voltage":121,"Current":0.000}} from topic: tele/taylor_water/SENSOR
 
-func sub(client mqtt.Client) {
-//	topic := "#"
-	topic := "tele/"+sensor+"/SENSOR"
-	token := client.Subscribe(topic, 1, nil)
-	token.Wait()
-	fmt.Printf("Subscribed to topic: %s\n", topic)
-}
